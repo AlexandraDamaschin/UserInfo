67267156c0c0ac710a4a05987c21b772dd72620b
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-02-23 18:23:38 +0000

    set image resource finded by id

--- app/src/main/java/com/example/android/userprofile/MainActivity.java
+++ app/src/main/java/com/example/android/userprofile/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.userprofile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        image view
+        ImageView picture = (ImageView) findViewById(R.id.profile_picture);
+        picture.setImageResource(R.drawable.profil_picture);
     }
 }
