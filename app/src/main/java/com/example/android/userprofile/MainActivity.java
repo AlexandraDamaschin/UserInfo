84b2c80b927e149d5891867a4640aa211a56ca1c
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-02-23 18:40:34 +0000

    text views finded by id

--- app/src/main/java/com/example/android/userprofile/MainActivity.java
+++ app/src/main/java/com/example/android/userprofile/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.android.userprofile;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
 //        image view
         ImageView picture = (ImageView) findViewById(R.id.profile_picture);
         picture.setImageResource(R.drawable.profil_picture);
+
+        //text view with name
+        TextView name= (TextView) findViewById(R.id.name);
+        name.setText("Alexandra Damaschin");
+
+        //text view with birthday date
+        TextView birthday = (TextView) findViewById(R.id.birthday);
+        birthday.setText("25 Sept 1996");
+
+        //text view with country
+        TextView country = (TextView) findViewById(R.id.country);
+        country.setText("Romania/Ireland");
     }
 }
